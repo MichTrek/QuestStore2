ebed92e79f68a4ff6c2aac90c84a0051cf3f4619
Trauke <piotrekenglert@gmail.com>
2019-01-16 15:20:37 +0100

    add update mentor

--- DB/src/main/java/DAO/AdminDAOInterface.java
+++ DB/src/main/java/DAO/AdminDAOInterface.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 public interface AdminDAOInterface {
     void createMentor(String first_name, String last_name, String email, String phone_number);
     void createClass(String class_name);
-    void editMentor();
+    void editMentor(String columnToChange, String update, int id);
     ResultSet showMentorById(int id);
     void createLevelOfExperience();
 }
