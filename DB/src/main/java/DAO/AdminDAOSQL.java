8b7135c96f5ad08353b1768aae1996e6ded132a0
Trauke <piotrekenglert@gmail.com>
2019-01-16 12:32:55 +0100

    add createMentor

--- DB/src/main/java/DAO/AdminDAOSQL.java
+++ DB/src/main/java/DAO/AdminDAOSQL.java
@@ -1,10 +1,27 @@
 package DAO;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AdminDAOSQL implements AdminDAOInterface{
+    DataBaseConnector dataBaseConnector = new DataBaseConnector();
     @Override
-    public void createMentor() {
+    public void createMentor(String first_name, String last_name, String email, String phone_number) {
+        String sql = "INSERT INTO mentors (first_name, last_name, email, phone_number) " +
+                "VALUES(?,?,?,?);";
+        try {
+            dataBaseConnector.connect();
+            PreparedStatement stmt = dataBaseConnector.getConnection().prepareStatement(sql);
+            stmt.setString(1,first_name);
+            stmt.setString(2,last_name);
+            stmt.setString(3,email);
+            stmt.setString(4,phone_number);
+            stmt.executeUpdate();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
 
     }
 
@@ -26,5 +43,9 @@ public class AdminDAOSQL implements AdminDAOInterface{
     @Override
     public void createLevelOfExperience() {
 
+    }
+    public static void main(String[] args) {
+        AdminDAOSQL adminDAOSQL = new AdminDAOSQL();
+        adminDAOSQL.createMentor("adam","maczek","asd@assa.pl","0700990880l");
     }
 }
